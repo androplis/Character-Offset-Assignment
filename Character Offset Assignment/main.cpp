5eda8120ecd438f4668df925d246aa2b5ecdc04c
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-12-02 11:10:45 -0500

    Created character function

--- Character Offset Assignment/main.cpp
+++ Character Offset Assignment/main.cpp
@@ -3,9 +3,26 @@
 // 12.2.2020
 
 #include <iostream>
+#include <cctype>
+#include <cstdlib>
 using namespace std;
 
+// == Function Prototypes
+char character(char, int);
+
 int main() {
+
     cout << endl << endl;
     return 0;
+}
+
+char character(char start, int offset) {
+    
+    if(!isalpha(start)) {
+        throw "invalidCharacterException";
+    }
+    else if (!isalpha(start + offset)) {
+        throw "invalidRangeException";
+    }
+    return start + offset;
 }
