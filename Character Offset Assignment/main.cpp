576ba159745033236be1801e19bfa6ab8e5347ed
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-12-02 11:28:45 -0500

    Implemented main driver

--- Character Offset Assignment/main.cpp
+++ Character Offset Assignment/main.cpp
@@ -15,18 +15,22 @@ class invalidCharacterException {};
 class invalidRangeException {};
 
 int main() {
+    const int numTests = 5;
     char letter;
-    try {
-        letter = character('A', 32);
-        cout << letter << endl;
-    }
-    catch (invalidCharacterException) {
-        cout << "ERROR: Invalid Character" << endl;
-    }
-    catch (invalidRangeException) {
-        cout << "ERROR: Invalid Range" << endl;
-    }
+    char tests[][numTests] = {{'a','a','Z','?','A'}, {1,-1,-1,5,32}};
     
+    for(int i = 0; i < numTests; i++) {
+        try {
+            letter = character(tests[0][i], tests[1][i]);
+            cout << letter << endl;
+        }
+        catch (invalidCharacterException) {
+            cout << "ERROR: Invalid Character" << endl;
+        }
+        catch (invalidRangeException) {
+            cout << "ERROR: Invalid Range" << endl;
+        }
+    }
     cout << endl << endl;
     return 0;
 }
@@ -36,7 +40,7 @@ char character(char start, int offset) {
     if(!isalpha(start)) {
         throw invalidCharacterException();
     }
-    else if (!isalpha(start + offset)) {
+    else if (!isalpha(tolower(start) + offset)) {
         throw invalidRangeException();
     }
     return start + offset;
